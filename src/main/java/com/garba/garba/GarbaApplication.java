5254bf084acec9040b5b083b0723e577560a4d11
shivammm21 <shivamthorat2103@gmail.com>
2025-08-25 13:09:23 +0530

    backend data added

---
+++ src/main/java/com/garba/garba/GarbaApplication.java
@@ -0,0 +1,13 @@
+package com.garba.garba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garba/garba/GarbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garba.garba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
